c580061d2d1d0669befc008bc3ddeeb65ced12ce
git-BR <bhrochamail@gmail.com>
2020-10-15 19:51:06 -0300

    :fire: cache cleaning

--- back-end-db/dist/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: 'd41d8cd98f00b204e9800998ecf8427e',
-        expiresIn: '1d',
-    },
-};
--- back-end-db/dist/database/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
-// createConnection method will automatically read connection options
-// from your ormconfig file or environment variables
-const connection = typeorm_1.createConnection();
